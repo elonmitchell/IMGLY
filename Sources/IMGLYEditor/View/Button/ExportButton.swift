1e080a108478f55b1d6624b2e44b48092ff24f4b
Elon Mitchell <iamtexasmade@gmail.com>
2024-08-24 19:51:33 -0500

    update export button

--- Sources/IMGLYEditor/View/Button/ExportButton.swift
+++ Sources/IMGLYEditor/View/Button/ExportButton.swift
@@ -8,11 +8,7 @@ import SwiftUI
   @_spi(Internal) public init() {}
 
   @_spi(Internal) public var body: some View {
-    Text("Export")
-      .onTapGesture {
-        // Restore the original export function call here
-        interactor.export()
-      }
+    ActionButton(.export)
       .disabled(interactor.isLoading
         || interactor.isExporting
         || (interactor.sceneMode == .video && interactor.timelineProperties.timeline?.totalDuration == .zero))
@@ -21,7 +17,6 @@ import SwiftUI
 
 struct ExportButton_Previews: PreviewProvider {
   static var previews: some View {
-    ExportButton()
-      .environmentObject(Interactor()) // Provide an environment object if needed
+    defaultPreviews
   }
 }
