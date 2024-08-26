c196546a7cb694e8160ae3feacecdaeeb8b4b5cd
Elon Mitchell <elon@elonandcompany.com>
2024-08-26 11:14:52 -0500

    uploaded

--- Sources/IMGLYEditor/View/Button/ExportButton.swift
+++ Sources/IMGLYEditor/View/Button/ExportButton.swift
@@ -3,26 +3,36 @@ import CoreMedia
 import SwiftUI
 
 @_spi(Internal) public struct ExportButton: View {
-  @EnvironmentObject private var interactor: Interactor
+    @EnvironmentObject private var interactor: Interactor
 
-  @_spi(Internal) public init() {}
+    @_spi(Internal) public init() {}
 
-  @_spi(Internal) public var body: some View {
-    Button(action: {
-        interactor.exportScene()
-    }) {
-        Image("custom.export.button", bundle: .module)
-            .font(.system(size: 24)) // Adjust the size as needed
-            .padding(8)
+    @_spi(Internal) public var body: some View {
+        Button(action: {
+            interactor.exportScene()
+        }) {
+            Image("custom.export.button", bundle: .module)
+                .renderingMode(.original) // Ensure the image displays in full color
+                .font(.system(size: 24)) // Adjust the size as needed
+                .padding(12)
+                .opacity(interactor.isLoading
+                         || interactor.isExporting
+                         || (interactor.sceneMode == .video && interactor.timelineProperties.timeline?.totalDuration == .zero) ? 0.5 : 1.0) // Reduce opacity when disabled
+                .overlay(
+                    interactor.isLoading
+                    || interactor.isExporting
+                    || (interactor.sceneMode == .video && interactor.timelineProperties.timeline?.totalDuration == .zero) ?
+                    Color.gray.opacity(0.4).blendMode(.overlay) : Color.clear
+                )
+        }
+        .disabled(interactor.isLoading
+                  || interactor.isExporting
+                  || (interactor.sceneMode == .video && interactor.timelineProperties.timeline?.totalDuration == .zero))
     }
-    .disabled(interactor.isLoading
-      || interactor.isExporting
-      || (interactor.sceneMode == .video && interactor.timelineProperties.timeline?.totalDuration == .zero))
-  }
 }
 
 struct ExportButton_Previews: PreviewProvider {
-  static var previews: some View {
-    ExportButton() // Provide the necessary environment object
-  }
+    static var previews: some View {
+        ExportButton() // Provide the necessary environment object
+    }
 }
