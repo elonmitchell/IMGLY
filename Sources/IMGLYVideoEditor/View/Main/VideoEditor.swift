32a70ed26f7dbabaa659741583500c0cea63340a
Elon Mitchell <elon@elonandcompany.com>
2024-08-26 06:28:50 -0500

    update

--- Sources/IMGLYVideoEditor/View/Main/VideoEditor.swift
+++ Sources/IMGLYVideoEditor/View/Main/VideoEditor.swift
@@ -22,7 +22,10 @@ public struct VideoEditor: View {
             .navigationTitle("")
             .toolbar {
                 ToolbarItemGroup(placement: .principal) {
-                    UndoRedoButtons()
+                    HStack(spacing: 8) { // Adjust spacing as needed
+                        UndoRedoButtons()
+                    }
+                    .frame(maxWidth: .infinity) // Ensures the HStack takes up all available space
                 }
                 ToolbarItemGroup(placement: .navigationBarTrailing) {
                     ExportButton()
